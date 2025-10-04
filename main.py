e351882d963811a85515445a07c2751047b532d0
marie <marie.kjelbergnes@gmail.com>
2025-10-04 14:19:16 +0200

    forbedret UI mye

--- main.py
+++ main.py
@@ -1,64 +1,69 @@
 import os
-import tkinter as tk
-from tkinter import ttk
 from tkinter import filedialog
 import read_bank
 import read_csv
 import re
 import datetime as dt
+import customtkinter as ctk
+import customcomponents as cc
 
 csv_data_path = "./csv_data/"
 
 
-class BaseApp(tk.Tk):
+class BaseApp(ctk.CTk):
     def __init__(self):
         super().__init__()
         self.files = []
 
         self.title("Økonomi Styring")
         self.geometry("1280x700")
+        ctk.set_appearance_mode("system")  # "light" or "dark"
+        ctk.set_default_color_theme("blue")  # colours
 
-        frame = ttk.Frame(self, padding=10)
+        frame = ctk.CTkFrame(self, corner_radius=10)
         frame.pack(fill="both", expand=True)
 
-        self.style = ttk.Style()
-        if "xpnative" in self.style.theme_names():
-            self.style.theme_use("xpnative")
-
-        self.title_label = ttk.Label(frame, text="Økonomi styring", font=("Helvetica", 24, "bold"))
+        self.title_label = ctk.CTkLabel(frame, text="Økonomi styring", font=("Helvetica", 24, "bold"))
         self.title_label.pack(pady=5)
 
-        self.left_frame = tk.Frame(frame)
+        self.left_frame = ctk.CTkFrame(frame, corner_radius=0, fg_color="transparent")
         self.left_frame.pack(side="left", fill="both", expand=True)
 
-        self.right_frame = tk.Frame(frame)
+        self.right_frame = ctk.CTkFrame(frame, corner_radius=0, fg_color="transparent")
         self.right_frame.pack(side="right", fill="both", expand=True)
 
-        self.status_label = ttk.Label(self.left_frame, text="Status")
+        self.status_label = ctk.CTkLabel(self.left_frame, text="Status")
         self.status_label.pack(pady=5)
-        self.status_text = tk.Text(self.left_frame, height=20, width=50)
+        self.status_text = ctk.CTkTextbox(self.left_frame, height=200, width=500)
         self.status_text.pack(pady=5)
 
-        self.processed_files_label = ttk.Label(self.right_frame, text="Prosesserte filer: 0")
+        self.processed_files_label = ctk.CTkLabel(self.right_frame, text="Prosesserte filer: 0")
         self.processed_files_label.pack(pady=5)
 
-        self.csv_files_btn = ttk.Button(self.right_frame, text="Velg CSVer", command=self.on_csv_button)
+        self.csv_files_btn = ctk.CTkButton(self.right_frame, text="Velg CSVer", command=self.on_csv_button)
         self.csv_files_btn.pack(pady=5)
 
-        self.clear_csv_btn = ttk.Button(self.right_frame, text="Slett prosesserte data", command=self.on_clear_csv)
+        self.clear_csv_btn = ctk.CTkButton(self.right_frame, text="Slett prosesserte data", command=self.on_clear_csv)
         self.clear_csv_btn.pack(pady=5)
 
-        self.set_dates_label = ttk.Label(self.right_frame, text="Velg dato (dd-mm-yyyy:dd-mm-yyyy):")
-        self.set_dates_label.pack(pady=5)
-        self.set_dates_input = ttk.Entry(self.right_frame)
-        self.set_dates_input.pack(pady=5)
+        self.start_date_label = ctk.CTkLabel(self.right_frame, text="Analyser fra dato (ddmmyyyy):")
+        self.start_date_label.pack(pady=5)
+        self.start_date = cc.CTkDateSelector(self.right_frame)
+        self.start_date.set_date(dt.date(2000, 1, 1))
+        self.start_date.pack(pady=5)
 
-        self.name_ignore_label = ttk.Label(self.right_frame, text="Navnet ditt (for å ignorere interne overføringer)")
+        self.end_date_label = ctk.CTkLabel(self.right_frame, text="Analyser til dato (ddmmyyyy):")
+        self.end_date_label.pack(pady=5)
+        self.end_date = cc.CTkDateSelector(self.right_frame)
+        self.end_date.set_date(dt.date(2100, 1, 1))
+        self.end_date.pack(pady=5)
+
+        self.name_ignore_label = ctk.CTkLabel(self.right_frame, text="Navnet ditt (for å ignorere interne overføringer)")
         self.name_ignore_label.pack(pady=5)
-        self.name_to_ignore = tk.Entry(self.right_frame)
+        self.name_to_ignore = ctk.CTkEntry(self.right_frame)
         self.name_to_ignore.pack(pady=5)
 
-        self.make_graphs_btn = ttk.Button(self.right_frame, text="Lag grafer!", command=self.on_make_graphs)
+        self.make_graphs_btn = ctk.CTkButton(self.right_frame, text="Lag grafer!", command=self.on_make_graphs)
         self.make_graphs_btn.pack(pady=5)
 
         self.update_processed_files()
@@ -70,7 +75,7 @@ class BaseApp(tk.Tk):
             if file_path.lower().endswith(".csv"):
                 processed_files += 1
 
-        self.processed_files_label.config(text=f"Prosesserte filer: {processed_files}")
+        self.processed_files_label.configure(text=f"Prosesserte filer: {processed_files}")
 
     def on_csv_button(self):
         files = filedialog.askopenfilenames(
@@ -78,13 +83,13 @@ class BaseApp(tk.Tk):
             filetypes=(("CSV-filer", "*.csv"), ("Alle filer", "*.*")))
 
         if not files:
-            self.status_text.insert(tk.END, "Du valgte ingen filer!\n")
+            self.status_text.insert(ctk.END, "Du valgte ingen filer!\n")
             return
         
         # read the files and convert them to csv's. 
         for file in files:
             status = read_bank.read_eika_csv(file)
-            self.status_text.insert(tk.END, str(status), "\n")
+            self.status_text.insert(ctk.END, str(status), "\n")
         
         self.update_processed_files()
 
@@ -94,22 +99,18 @@ class BaseApp(tk.Tk):
             if file_path.lower().endswith(".csv"):
                 os.remove(file_path)
         
-        self.status_text.insert(tk.END, "Slettet alle prosesserte data!\n")
+        self.status_text.insert(ctk.END, "Slettet alle prosesserte data!\n")
         self.update_processed_files()
 
     def on_make_graphs(self):
         """ Makes graphs (or something) """
-        date_text = self.set_dates_input.get()
-        dates = re.findall(r"(\d{2})-(\d{2})-(\d{4}):(\d{2})-(\d{2})-(\d{4})", date_text)
-        duration = (dt.datetime(1970, 1, 1), dt.datetime(2100, 1, 1))
-        if len(dates) == 1:
-            dates = dates[0]
-            dates = [int(d) for d in dates]
-            dates.reverse()  # dd mm yyyy, dd2 mm2 yyyy2 --> yyyy2 mm2 dd2, yyyy mm dd
-            start_date = dt.datetime(*dates[3:])
-            end_date = dt.datetime(*dates[:3])
-            if end_date > start_date:
-                duration = (start_date, end_date)
+        self.status_text.insert(ctk.END, "Lager grafer!")
+
+        start_date = self.start_date.get_date()
+        end_date = self.end_date.get_date()
+        duration = (dt.date(1970, 1, 1), dt.date(2100, 1, 1))
+        if end_date > start_date:
+            duration = (start_date, end_date)
 
         # honestly, this could be better (it's very redudant)
         files = []
@@ -121,7 +122,7 @@ class BaseApp(tk.Tk):
         if len(files) != 0:
             read_csv.graph_everything(files, duration, self.name_to_ignore.get())
         else:
-            self.status_text.insert(tk.END, "Kunne ikke lage grafer, da ingen filer er prosesserte!\n")
+            self.status_text.insert(ctk.END, "Kunne ikke lage grafer, da ingen filer er prosesserte!\n")
 
 
 def main():
